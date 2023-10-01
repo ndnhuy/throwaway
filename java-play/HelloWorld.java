1854ad809a9659cf0054c70d96fc8f00c5c39483
Huy Nguyen <ndnhuy2504@gmail.com>
2023-10-01 21:44:25 +0700

    java play

---
+++ java-play/HelloWorld.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("hello world hahaha I make it");
+  }
+
+  @Test
+  public void testFoo() {
+  }
+}
